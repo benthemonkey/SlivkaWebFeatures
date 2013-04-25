9dcaf6fd3f154bbe93ccf1c454e8d5bc02b6aeff
Ben Rothman <bensrothman@gmail.com>
2013-04-24 20:02:00 -0500

    quick fixes

--- ajax/sendPointsCorrection.php
+++ ajax/sendPointsCorrection.php
@@ -2,6 +2,7 @@
 header('Content-type: text/html; charset=utf-8');
 require_once "./PointsCenter.php";
 $points_center = new PointsCenter();
+$key = md5($_GET['event_name'] . $_GET['name']);
 
 $message = $points_center->submitPointsCorrectionForm($_GET,$key);
 
