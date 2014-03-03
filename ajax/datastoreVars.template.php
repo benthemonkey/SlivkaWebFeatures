ac92e663d328472ad2592c38c6bb3441ba2f75c4
Ben Rothman <bensrothman@gmail.com>
2014-03-02 21:43:08 -0600

    fixes for transition of powers. adding template datastore file

---
+++ ajax/datastoreVars.template.php
@@ -0,0 +1,19 @@
+<?php
+$DB_TYPE = "mysql";
+$DB_HOST = "localhost";
+$DB_NAME = "slivka";
+$DB_USER = "root";
+$DB_PASS = "root";
+
+$VP_NAME = "FirstName LastName";
+$VP_EMAIL = "emailaddress@service.com";
+$VP_EMAIL_PASS = "secret"; #must be a "temporary" password
+$VP_EMAIL_BOT = "emailaddress+mailbot@service.com";
+$VP_EMAIL_POINT_SUBMISSION_NOTIFICATIONS = true;
+
+$QTR = 1401; #this is the only place where quarter must be updated
+$HOUSING_MALES = 43;
+$HOUSING_FEMALES = 39;
+$IS_HOUSING = false;
+
+?>
