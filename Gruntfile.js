2340279d25c6c3786d6449d376ced3346e0bd8a9
benthemonkey <bensrothman@gmail.com>
2014-06-14 16:51:26 -0500

    add font-awesome

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function(grunt) {
 					'css/<%= pkg.name %>.built.css': [
 						'css/bootstrap.css',
 						'css/bootstrap-theme.css',
+						'css/font-awesome.css',
 						'css/typeahead.js-bootstrap.css',
 						'css/nprogress.css',
 						'css/bootstrap-multiselect.css',
