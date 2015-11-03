deadf7168bb85f55626c13632a63f1d1b7f5fe8f
Ben Rothman <bensrothman@gmail.com>
2015-11-02 22:33:27 -0600

    good-enough fix for directory page

--- js/directory.js
+++ js/directory.js
@@ -1,49 +1,60 @@
 /* eslint strict:0 */
 
-$(function() {
+jQuery(function() {
     'use strict';
 
-    $.getJSON('/points/ajax/getDirectory.php', function(data) {
-        var i;
-        var path = 'http://slivka.northwestern.edu/points/img/slivkans/';
-
-        for (i = 0; i < data.length; i++) {
-            data[i][5] = [
-                '<img class="directoryphoto" src="', path, data[i][5], '"',
-                'title="', data[i][0], ' ', data[i][1], '"',
-                'style="height: 100px; display: none;" />'
-            ].join('');
+    var checkForDataTables = function() {
+        if (typeof($().DataTable) === 'undefined') {
+            setTimeout(checkForDataTables, 100);
+        } else {
+            init();
         }
+    },
+    init = function() {
+        $.getJSON('/points/ajax/getDirectory.php', function(data) {
+            var i;
+            var path = 'http://slivka.northwestern.edu/points/img/slivkans/';
 
-        $('#directory').dataTable( {
-            data: data,
-            columns: [
-            { title: 'First Name' },
-            { title: 'Last Name' },
-            { title: 'Year' },
-            { title: 'Major' },
-            { title: 'Suite' },
-            { title: 'Photo', orderable: false }
-            ],
-            order: [[0, 'asc']],
-            paging: false
-        });
+            for (i = 0; i < data.length; i++) {
+                data[i][5] = [
+                    '<img class="directoryphoto" src="', path, data[i][5], '"',
+                    'title="', data[i][0], ' ', data[i][1], '"',
+                    'style="height: 100px; display: none;" />'
+                ].join('');
+            }
 
-        // directory password
-        // This is a very insecure measure, mostly so the photos
-        // aren't displayed by default and you have to know javascript to display them
-        $('#submitdirectorypass').click(function() {
-            if ('discoverslivka' === $('#directorypass').val()) {
-                $('.directoryphoto').show();
+            $('#directory').dataTable( {
+                data: data,
+                columns: [
+                { title: 'First Name' },
+                { title: 'Last Name' },
+                { title: 'Year' },
+                { title: 'Major' },
+                { title: 'Suite' },
+                { title: 'Photo', orderable: false }
+                ],
+                order: [[0, 'asc']],
+                paging: false
+            });
 
-                // dumb but works: saving password in localstorage
-                localStorage.directorypass = 'discoverslivka';
+            // directory password
+            // This is a very insecure measure, mostly so the photos
+            // aren't displayed by default and you have to know javascript to display them
+            $('#submitdirectorypass').click(function() {
+                if ('discoverslivka' === $('#directorypass').val()) {
+                    $('.directoryphoto').show();
+
+                    // dumb but works: saving password in localstorage
+                    localStorage.directorypass = 'discoverslivka';
+                }
+            });
+
+            if (localStorage.directorypass) {
+                $('#directorypass').val(localStorage.directorypass);
+                $('#submitdirectorypass').click();
             }
         });
+    }
 
-        if (localStorage.directorypass) {
-            $('#directorypass').val(localStorage.directorypass);
-            $('#submitdirectorypass').click();
-        }
-    });
+    $.getScript('/points/node_modules/datatables/media/js/jquery.dataTables.min.js', checkForDataTables);
 });
